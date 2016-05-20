d293cb2b221a9609ea01e13eb2f26ab0b3f030e0
atomicguy <adam@atompowered.net>
2016-05-20 00:31:24 -0400

    initial play/pause support

--- index.js
+++ index.js
@@ -7,23 +7,28 @@ if (typeof AFRAME === 'undefined') {
  */
 AFRAME.registerComponent('scrim', {
     schema: {
-        default: true
+        timer: { type: 'int', default: 700 }
     },
 
     init: function() {
-        var thisEl = this.el;
-        var scrim = document.createElement('a-animation');
+        document.querySelector('a-scene').addEventListener('enter-vr', this.pause(this));
+        document.querySelector('a-scene').addEventListener('exit-vr', this.play);
 
-        thisEl.appendChild(scrim);
-
-        scrim.setAttribute('attribute', 'material.opacity');
-        scrim.setAttribute('dur', '700');
-        scrim.setAttribute('direction', 'alternate');
-        scrim.setAttribute('ease', 'ease-in-out');
-        scrim.setAttribute('fill', 'forwards');
-        scrim.setAttribute('from', '0');
-        scrim.setAttribute('to', '1');
-        scrim.setAttribute('repeat', 'indefinite');
+        // var thisEl = this.el;
+        // var scrim = document.createElement('a-animation');
+        //
+        // var timing = this.data.timer;
+        //
+        // thisEl.appendChild(scrim);
+        //
+        // scrim.setAttribute('attribute', 'material.opacity');
+        // scrim.setAttribute('dur', timing);
+        // scrim.setAttribute('direction', 'alternate');
+        // scrim.setAttribute('ease', 'ease-in-out');
+        // scrim.setAttribute('fill', 'forwards');
+        // scrim.setAttribute('from', '0');
+        // scrim.setAttribute('to', '1');
+        // scrim.setAttribute('repeat', 'indefinite');
     },
 
     remove: function() {
@@ -31,10 +36,40 @@ AFRAME.registerComponent('scrim', {
     },
 
     play: function() {
+        // var target = document.querySelector('a-entity');
+        // var animation = document.createElement('a-animation');
+        //
+        // target.appendChild(animation);
 
+        var thisEl = this.el;
+        var scrim = document.createElement('a-animation');
+
+        var timing = this.data.timer;
+
+        thisEl.appendChild(scrim);
+
+        scrim.setAttribute('attribute', 'material.opacity');
+        scrim.setAttribute('ease', 'ease-in-out');
+        scrim.setAttribute('direction', 'alternate');
+        scrim.setAttribute('dur', timing);
+        scrim.setAttribute('fill', 'forwards');
+        scrim.setAttribute('from', '1');
+        scrim.setAttribute('to', '0');
+        scrim.setAttribute('repeat', 'indefinite');
     },
 
     pause: function() {
+        // var animation = document.querySelector('a-animation');
+        // var target = animation.parentNode;
+
+        var scrim = this.el.children;
+
+        console.log(scrim);
+
+
+        // var thisEl = this;
+        // console.log('pause triggered');
+        // console.log(thisEl);
 
     },
 
